82e152c426a66b3cc14bb293e883190294e43e72
Doji <stijn.c.dejongh@gmail.com>
2019-05-13 20:45:25 +0200

    Completion of FileWriter implementation

--- trambu-infrastructure/src/main/java/be/doji/productivity/trambu/infrastructure/file/FileWriter.java
+++ trambu-infrastructure/src/main/java/be/doji/productivity/trambu/infrastructure/file/FileWriter.java
@@ -1,17 +1,36 @@
 package be.doji.productivity.trambu.infrastructure.file;
 
+import be.doji.productivity.trambu.infrastructure.converter.ActivityConverter;
+import be.doji.productivity.trambu.infrastructure.converter.ActivityDataConverter;
+import be.doji.productivity.trambu.infrastructure.repository.ActivityDatabaseRepository;
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class FileWriter {
 
-  public void writeActivtiesToFile(File file) {
+  private final ActivityDatabaseRepository activityRepository;
+
+  public FileWriter(@Autowired ActivityDatabaseRepository repository) {
+    this.activityRepository = repository;
+  }
+
+  public void writeActivtiesToFile(File file) throws IOException {
     writeActivtiesToFile(file.toPath());
   }
 
-  public void writeActivtiesToFile(Path path) {
-
+  public void writeActivtiesToFile(Path path) throws IOException {
+    List<String> activities = activityRepository.findAll().
+        stream()
+        .map(ActivityDataConverter::parse)
+        .map(ActivityConverter::write)
+        .collect(Collectors.toList());
+    Files.write(path, activities);
   }
 }
