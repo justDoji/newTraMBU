17ed39c9d7e9b7efdb1c3a3cadb5233db2b7b8c0
Doji <stijn.c.dejongh@gmail.com>
2019-05-21 18:44:06 +0200

    Add tests for ActivityController

--- trambu-front/src/test/java/be/doji/productivity/trambu/front/controller/ActivityOverviewControllerIntegrationTest.java
+++ trambu-front/src/test/java/be/doji/productivity/trambu/front/controller/ActivityOverviewControllerIntegrationTest.java
@@ -3,23 +3,23 @@
  *
  * Copyright (c) 2019 Stijn Dejongh
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU Affero General Public License as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Affero General Public License for more details.
  *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Affero General Public License along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
  */
 package be.doji.productivity.trambu.front.controller;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import be.doji.productivity.trambu.front.transfer.ActivityModel;
 import be.doji.productivity.trambu.infrastructure.file.FileLoader;
 import be.doji.productivity.trambu.infrastructure.file.FileWriter;
 import be.doji.productivity.trambu.infrastructure.repository.ActivityDatabaseRepository;
@@ -32,7 +32,6 @@ import java.util.Arrays;
 import org.assertj.core.api.Assertions;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -81,28 +80,24 @@ public class ActivityOverviewControllerIntegrationTest {
     File file = new File(classLoader.getResource("controller/todo_write.txt").toURI());
     controller.setTodoFile(file);
     controller.clearActivities();
-    Assertions.assertThat(controller.getActivities()).isEmpty();
+    assertThat(controller.getActivities()).isEmpty();
 
     controller.createActivity();
 
-    Assertions.assertThat(controller.getActivities()).hasSize(1);
+    assertThat(controller.getActivities()).hasSize(1);
   }
 
   @Test
   public void saveActivities_writeToRepository() throws URISyntaxException {
-    ClassLoader classLoader = getClass().getClassLoader();
-    File file = new File(classLoader.getResource("controller/todo_write.txt").toURI());
-    controller.setTodoFile(file);
-    controller.clearActivities();
-    repository.deleteAll();
-    Assertions.assertThat(controller.getActivities()).isEmpty();
-    Assertions.assertThat(repository.findAll()).isEmpty();
+    clearActivityState();
+    assertThat(controller.getActivities()).isEmpty();
+    assertThat(repository.findAll()).isEmpty();
 
     controller.createActivity();
-    Assertions.assertThat(controller.getActivities()).hasSize(1);
+    assertThat(controller.getActivities()).hasSize(1);
 
     controller.saveActivities();
-    Assertions.assertThat(repository.findAll()).hasSize(1);
+    assertThat(repository.findAll()).hasSize(1);
   }
 
   @Test
@@ -112,19 +107,19 @@ public class ActivityOverviewControllerIntegrationTest {
     controller.clearActivities();
     repository.deleteAll();
     controller.setTodoFile(file);
-    Assertions.assertThat(controller.getActivities()).isEmpty();
-    Assertions.assertThat(Files.readAllLines(file.toPath())).isEmpty();
+    assertThat(controller.getActivities()).isEmpty();
+    assertThat(Files.readAllLines(file.toPath())).isEmpty();
 
     controller.createActivity();
-    Assertions.assertThat(controller.getActivities()).hasSize(1);
+    assertThat(controller.getActivities()).hasSize(1);
 
     controller.saveActivities();
-    Assertions.assertThat(Files.readAllLines(file.toPath())).hasSize(1);
+    assertThat(Files.readAllLines(file.toPath())).hasSize(1);
   }
 
 
-  private static final String LINE_ONE ="x (A) [Go to store for food?] @[food] due:2018-12-07:00:00:00.000 warningPeriod:PT24H";
-  private static final String LINE_TWO ="(C) [Show the application to people] +[TraMBU] @[showoff] warningPeriod:PT24H loc:[Home]";
+  private static final String LINE_ONE = "x (A) [Go to store for food?] @[food] due:2018-12-07:00:00:00.000 warningPeriod:PT24H";
+  private static final String LINE_TWO = "(C) [Show the application to people] +[TraMBU] @[showoff] warningPeriod:PT24H loc:[Home]";
   private static final String LINE_THREE = "(A) 2017-10-21:14:13.000 [Hello World!] +[Overarching Project] @[Tag] @[Tag with multiple words] due:2017-12-21:16:15:00.000 uuid:283b6271-b513-4e89-b757-10e98c9078ea";
 
   @After
@@ -135,6 +130,92 @@ public class ActivityOverviewControllerIntegrationTest {
 
     file = new File(classLoader.getResource("controller/todo_read.txt").toURI());
     Files.write(file.toPath(), Arrays.asList(LINE_ONE, LINE_TWO, LINE_THREE));
+  }
+
+  @Test
+  public void toggleEditable_savesActivities() throws URISyntaxException {
+    clearActivityState();
+    assertThat(controller.getActivities()).isEmpty();
+    assertThat(repository.findAll()).isEmpty();
+
+    controller.createActivity();
+    assertThat(controller.getActivities()).hasSize(1);
+
+    // Replicate UI interactions
+    ActivityModel toToggle = controller.getActivities().get(0);
+    controller.toggleEditable(toToggle);
+    toToggle.setTitle("A new title");
+    controller.toggleEditable(toToggle);
+
+    assertThat(repository.findAll()).hasSize(1);
+    assertThat(repository.findAll().get(0).getTitle()).isEqualTo("A new title");
+  }
+
+  @Test
+  public void toggleComplete_savesActivities() throws URISyntaxException {
+    clearActivityState();
+    assertThat(controller.getActivities()).isEmpty();
+    assertThat(repository.findAll()).isEmpty();
+
+    controller.createActivity();
+    assertThat(controller.getActivities()).hasSize(1);
+
+    // Replicate UI interactions
+    ActivityModel toToggle = controller.getActivities().get(0);
+    assertThat(toToggle.isCompleted()).isFalse();
+
+    controller.toggleCompleted(toToggle);
+
+    assertThat(repository.findAll()).hasSize(1);
+    assertThat(repository.findAll().get(0).isCompleted()).isTrue();
+  }
+
+  @Test
+  public void completeTags_containsAllExistingTags() throws URISyntaxException {
+    clearActivityState();
+    assertThat(controller.getActivities()).isEmpty();
+    assertThat(repository.findAll()).isEmpty();
+
+    controller.createActivity();
+    ActivityModel activityOne = controller.getActivities().get(0);
+    activityOne.setTags(Arrays.asList("Cone", "Two"));
+
+    controller.createActivity();
+    ActivityModel activityTwo = controller.getActivities().get(1);
+    activityTwo.setTags(Arrays.asList("Cat", "Dog"));
+
+    assertThat(controller.completeTags("C")).hasSize(2);
+    assertThat(controller.completeTags("D")).hasSize(1);
+    assertThat(controller.completeTags("T")).hasSize(2);
+  }
+
+  @Test
+  public void completeProjects_containsAllExistingTags() throws URISyntaxException {
+    clearActivityState();
+    assertThat(controller.getActivities()).isEmpty();
+    assertThat(repository.findAll()).isEmpty();
+
+    controller.createActivity();
+    ActivityModel activityOne = controller.getActivities().get(0);
+    activityOne.setProjects(Arrays.asList("Cone", "Two"));
+
+    controller.createActivity();
+    ActivityModel activityTwo = controller.getActivities().get(1);
+    activityTwo.setProjects(Arrays.asList("Cat", "Dog"));
+
+    assertThat(controller.completeProjects("C")).hasSize(2);
+    assertThat(controller.completeProjects("D")).hasSize(1);
+    assertThat(controller.completeProjects("T")).hasSize(2);
+  }
+
+
+
+  private void clearActivityState() throws URISyntaxException {
+    ClassLoader classLoader = getClass().getClassLoader();
+    File file = new File(classLoader.getResource("controller/todo_write.txt").toURI());
+    controller.setTodoFile(file);
+    controller.clearActivities();
+    repository.deleteAll();
   }
 
 
