5437a176ac75950474432873b4ba25924f79d7fd
Doji <stijn.c.dejongh@gmail.com>
2019-05-21 19:31:08 +0200

    Apply sonarqube suggestion

--- trambu-front/src/main/java/be/doji/productivity/trambu/front/filter/FilterChain.java
+++ trambu-front/src/main/java/be/doji/productivity/trambu/front/filter/FilterChain.java
@@ -3,30 +3,29 @@
  *
  * Copyright (c) 2019 Stijn Dejongh
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU Affero General Public License as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Affero General Public License for more details.
  *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Affero General Public License along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
  */
 package be.doji.productivity.trambu.front.filter;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class FilterChain<T> {
 
   private List<PositiveFilter> filters = new ArrayList<>();
 
-  public <P> void addPositiveFiler(Function<T, P> supplier, Function<P, Boolean> includeWhen) {
+  public <P> void addPositiveFiler(Function<T, P> supplier, Predicate<P> includeWhen) {
     this.filters.add(new PositiveFilter(supplier, includeWhen));
   }
 
@@ -50,15 +49,15 @@ public class FilterChain<T> {
   class PositiveFilter<T, P> {
 
     private final Function<T, P> supplier;
-    private final Function<P, Boolean> isTobeAddedWhen;
+    private final Predicate<P> isTobeAddedWhen;
 
-    public PositiveFilter(Function<T, P> supplier, Function<P, Boolean> isTobeAddedWhen) {
+    public PositiveFilter(Function<T, P> supplier, Predicate<P> isTobeAddedWhen) {
       this.isTobeAddedWhen = isTobeAddedWhen;
       this.supplier = supplier;
     }
 
     public boolean allows(T elementToCheck) {
-      return isTobeAddedWhen.apply(supplier.apply(elementToCheck));
+      return isTobeAddedWhen.test(supplier.apply(elementToCheck));
     }
   }
 
