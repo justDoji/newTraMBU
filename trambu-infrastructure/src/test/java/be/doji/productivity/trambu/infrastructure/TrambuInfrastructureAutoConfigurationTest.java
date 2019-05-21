eae6796d89b7dd03e6c68974ae1961c30121d733
Doji <stijn.c.dejongh@gmail.com>
2019-05-21 18:47:15 +0200

    Cleanup

--- trambu-infrastructure/src/test/java/be/doji/productivity/trambu/infrastructure/TrambuInfrastructureAutoConfigurationTest.java
+++ trambu-infrastructure/src/test/java/be/doji/productivity/trambu/infrastructure/TrambuInfrastructureAutoConfigurationTest.java
@@ -3,24 +3,22 @@
  *
  * Copyright (c) 2019 Stijn Dejongh
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU Affero General Public License as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Affero General Public License for more details.
  *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Affero General Public License along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
  */
 package be.doji.productivity.trambu.infrastructure;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import be.doji.productivity.trambu.infrastructure.repository.ActivityRepository;
+import be.doji.productivity.trambu.infrastructure.repository.ActivityDatabaseRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,7 +32,7 @@ public class TrambuInfrastructureAutoConfigurationTest {
 
   @Autowired
   @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
-  private ActivityRepository autowiredService;
+  private ActivityDatabaseRepository autowiredService;
 
   @Test
   public void autowiringWorks() {
