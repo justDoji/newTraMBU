ccee5ff38de06208ff96b6ed1327ff4555cf8e18
Doji <25401297+justDoji@users.noreply.github.com>
2019-06-05 12:15:09 +0200

    More sonar issues

--- trambu-domain/src/test/java/be/doji/productivity/trambu/domain/time/TimeSlotTest.java
+++ trambu-domain/src/test/java/be/doji/productivity/trambu/domain/time/TimeSlotTest.java
@@ -1,23 +1,21 @@
 /**
  * TraMBU - an open time management tool
  *
- *     Copyright (C) 2019  Stijn Dejongh
+ * Copyright (C) 2019  Stijn Dejongh
  *
- *     This program is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU Affero General Public License as
- *     published by the Free Software Foundation, either version 3 of the
- *     License, or (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU Affero General Public License as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
  *
- *     This program is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU Affero General Public License for more details.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Affero General Public License for more details.
  *
- *     You should have received a copy of the GNU Affero General Public License
- *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Affero General Public License along with this program.
+ * If not, see <https://www.gnu.org/licenses/>.
  *
- *     For further information on usage, or licensing, contact the author
- *     through his github profile: https://github.com/justDoji
+ * For further information on usage, or licensing, contact the author through his github profile:
+ * https://github.com/justDoji
  */
 package be.doji.productivity.trambu.domain.time;
 
@@ -29,7 +27,6 @@ import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
-import org.assertj.core.api.Assertions;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -121,7 +118,7 @@ public class TimeSlotTest {
 
     TimeSlot slot = TimeSlot.between(startTime.getTime(), endTime.getTime());
 
-    Assertions.assertThat(slot.getTimeSpanInHours()).isEqualTo(192.5);
+    assertThat(slot.getTimeSpanInHours()).isEqualTo(192.5);
   }
 
   @Test
@@ -136,7 +133,7 @@ public class TimeSlotTest {
 
     TimeSlot slot = TimeSlot.between(startTime.getTime(), endTime.getTime());
     TimeSlot overlap = slot.overlapWithToday();
-    Assertions.assertThat(overlap.getTimeSpanInHours()).isEqualTo(14.5);
+    assertThat(overlap.getTimeSpanInHours()).isEqualTo(14.5);
   }
 
   @After
