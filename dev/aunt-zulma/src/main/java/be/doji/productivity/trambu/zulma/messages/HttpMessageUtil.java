8a33b4349176bbb09f692b7dad8670b64367b47e
Stijn Dejongh <25401297+justDoji@users.noreply.github.com>
2019-12-22 14:47:17 +0100

    Add REST sender functionality to Aunt Zulma
    - Refactor existing code

---
+++ dev/aunt-zulma/src/main/java/be/doji/productivity/trambu/zulma/messages/HttpMessageUtil.java
@@ -0,0 +1,65 @@
+package be.doji.productivity.trambu.zulma.messages;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public final class HttpMessageUtil {
+
+  private HttpMessageUtil() {
+    throw new UnsupportedOperationException("Utility classes should not be instantiated");
+  }
+
+  public static byte[] toBytes(String fullMessage) throws IOException {
+    ByteArrayOutputStream bout = new ByteArrayOutputStream();
+
+    byte[] buffer = fullMessage.getBytes();
+    bout.write(buffer);
+    return bout.toByteArray();
+  }
+
+  public static HttpURLConnection createHttpConnection(String endpoint) throws IOException {
+    URL url = new URL(endpoint);
+    URLConnection connection = url.openConnection();
+    return (HttpURLConnection) connection;
+  }
+
+  public static void sendHttpRequest(
+      HttpURLConnection httpConn, Map<String, String> requestProperties,
+      String requestMethod, byte[] bytesToSend)
+      throws IOException {
+    for (Entry<String, String> property : requestProperties.entrySet()) {
+      httpConn.setRequestProperty(property.getKey(), property.getValue());
+    }
+    httpConn.setRequestMethod(requestMethod);
+    httpConn.setDoOutput(true);
+    httpConn.setDoInput(true);
+
+    OutputStream out = httpConn.getOutputStream();
+
+    out.write(bytesToSend);
+    out.close();
+  }
+
+  public static String readHttpResponse(HttpURLConnection httpConn) throws IOException {
+
+    InputStreamReader isr =
+        new InputStreamReader(httpConn.getInputStream());
+    BufferedReader in = new BufferedReader(isr);
+
+    StringBuilder outputString = new StringBuilder();
+    String responseString = "";
+    while ((responseString = in.readLine()) != null) {
+      outputString.append(responseString);
+    }
+    return outputString.toString();
+  }
+
+}
--- dev/aunt-zulma/src/main/java/be/doji/productivity/trambu/zulma/rest/RestSender.java
+++ dev/aunt-zulma/src/main/java/be/doji/productivity/trambu/zulma/rest/RestSender.java
@@ -1,4 +1,4 @@
-package be.doji.productivity.trambu.zulma.soap;
+package be.doji.productivity.trambu.zulma.rest;
 
 import static be.doji.productivity.trambu.zulma.messages.HttpMessageUtil.createHttpConnection;
 import static be.doji.productivity.trambu.zulma.messages.HttpMessageUtil.readHttpResponse;
@@ -9,32 +9,30 @@ import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.util.HashMap;
 import java.util.Map;
-import org.jetbrains.annotations.NotNull;
+import lombok.NonNull;
 
-public class SoapSender {
+public class RestSender {
 
-  public String sendRequest(String endpoint, String soapAction, String fullMessage)
+  public String sendRequest(String endpoint, String fullMessage, String messageAction)
       throws IOException {
     HttpURLConnection httpConn = createHttpConnection(endpoint);
 
     sendHttpRequest(
         httpConn,
-        createSoapHeaders(toBytes(fullMessage), soapAction),
-        "POST",
+        createRestHeader(toBytes(fullMessage)),
+        messageAction,
         toBytes(fullMessage)
     );
 
     return readHttpResponse(httpConn);
   }
 
-  @NotNull
-  private Map<String, String> createSoapHeaders(byte[] bytesToWrite, String SOAPAction) {
+  @NonNull
+  private Map<String, String> createRestHeader(byte[] bytesToWrite) {
     Map<String, String> connectionProperties = new HashMap<>();
+    connectionProperties.put("Content-Type", "application/json; charset=utf-8");
+    connectionProperties.put("Transfer-Encoding", "chunked");
     connectionProperties.put("Content-Length", String.valueOf(bytesToWrite.length));
-    connectionProperties.put("Content-Type", "text/xml; charset=utf-8");
-    connectionProperties.put("SOAPAction", SOAPAction);
     return connectionProperties;
   }
-
-
 }
