b31e10eaf24c4c1ff9f022cc1ce31323a0349698
Stijn Dejongh <25401297+justDoji@users.noreply.github.com>
2019-12-23 15:26:42 +0100

    Clean imports

--- dev/planning/src/main/java/be/doji/productivity/trambu/planning/domain/Action.java
+++ dev/planning/src/main/java/be/doji/productivity/trambu/planning/domain/Action.java
@@ -1,23 +1,21 @@
 /**
  * TraMBU - an open time management tool
- *
- *     Copyright (C) 2019  Stijn Dejongh
- *
- *     This program is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU Affero General Public License as
- *     published by the Free Software Foundation, either version 3 of the
- *     License, or (at your option) any later version.
- *
- *     This program is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU Affero General Public License for more details.
- *
- *     You should have received a copy of the GNU Affero General Public License
- *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
- *
- *     For further information on usage, or licensing, contact the author
- *     through his github profile: https://github.com/justDoji
+ * <p>
+ * Copyright (C) 2019  Stijn Dejongh
+ * <p>
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU Affero General Public License as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ * <p>
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Affero General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU Affero General Public License along with this program.
+ * If not, see <https://www.gnu.org/licenses/>.
+ * <p>
+ * For further information on usage, or licensing, contact the author through his github profile:
+ * https://github.com/justDoji
  */
 package be.doji.productivity.trambu.planning.domain;
 
@@ -91,7 +89,7 @@ public class Action {
   }
 
   public void addComment(Comment comment) {
-    if(comment != null && !this.comments.contains(comment)) {
+    if (comment != null && !this.comments.contains(comment)) {
       this.comments.add(comment);
     }
   }
